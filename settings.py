3d63bbd4a517a9cf78aa94a387e2a80ee4d721bf
Aleksandar Rusinov <Aleksandar Rusinov>
2017-03-31 17:45:29 +0100

    Default accuracy labels

--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
 # common settings for results analysis
 AMT_FILE = '100_sentence_9_error_detection.csv'
+AMT_SENTENCE_BATCH = 'amt_sentence_batch.csv'
+AMT_FCE_M2 = 'fce_amt.experiment_two.max.rasp.m2'
 TRAINING_DATA_FILE = 'fce_train.gold.max.rasp.old_cat.m2'
 RESPONSE_COUNT = 9
+ACCURACY_LABELS = {'xlabel': 'judgements', 'ylabel': 'accuracies'}
